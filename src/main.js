a2eb4090aafa948e22a53fc3adc0be37b94bbc5e
ssssstudy8 <ssssstudy@fomail.com>
2022-07-15 12:09:32 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {
+  Button
+} from 'element-ui';
+import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.component('el-button', Button)
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
